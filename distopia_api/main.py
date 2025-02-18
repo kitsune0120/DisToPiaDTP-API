75dbedcb44b0b57b4c2a919538f627caa0517bde
kitsune0120 <kitsunee0120@gmail.com>
2025-02-19 04:42:36 +0900

    Update main.py

--- distopia_api/main.py
+++ distopia_api/main.py
@@ -1,7 +1,7 @@
 import shutil
 from fastapi import FastAPI, Depends, File, UploadFile, HTTPException
 from sqlalchemy.orm import Session
-from database import engine, Base, get_db
+from distopia_api.database import engine, Base, get_db
 from pathlib import Path
 import models
 import openai
