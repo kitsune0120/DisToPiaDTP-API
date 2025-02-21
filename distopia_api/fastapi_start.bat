8b9c85d789eb2ed7f267034cf9fa4ef3e6c0b6ad
kitsune0120 <kitsunee0120@gmail.com>
2025-02-22 07:05:39 +0900

    충돌 해결

--- distopia_api/fastapi_start.bat
+++ distopia_api/fastapi_start.bat
@@ -1,3 +1,11 @@
-@echo off 
-cd /d D:\DTP\DTP\데이터베이스\distopia_api 
-py -3.13 -m uvicorn main:app --reload 
+@echo off
+echo Before cd: %cd%
+
+cd /d "D:\DTP\DTP\data\distopia_api"
+echo After cd: %cd%
+
+call venv\Scripts\activate.bat
+echo Virtual Environment Activated: %VIRTUAL_ENV%
+
+uvicorn main:app --reload
+pause
